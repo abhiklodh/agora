30f44f525afdd7c0d5a5c02ac1134221678f9a12
Pascal Wengerter <pascal@wengerter.info>
2020-03-21 10:36:56 +0100

    Add devise setup & basic user model & signup/registration form

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Agora
+
+## Development
+
+Setup: `docker-compose build`
+
+Run: `docker-compose up --remove-orphans`
+
+# Deployment
+
+Sour code: `git push heroku master`
+
+Migrations: `heroku run rake db:migrate`
--- app/matestack/pages/user_app/example_page.rb
+++ app/matestack/pages/user_app/example_page.rb
@@ -3,11 +3,8 @@ class Pages::UserApp::ExamplePage < Matestack::Ui::Page
   def response
     components {
       div do
-        plain "hello world!"
-        5.times do
-          br
-          plain "Hurray!"
-        end
+        plain "please create an account"
+        link path: :new_user_registration_path, text: 'Sign up'
       end
     }
   end
