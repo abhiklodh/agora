2d3f1c3773ec88ce6c78127d1e47278ae254c2ae
Pascal Wengerter <pascal@wengerter.info>
2020-03-22 19:03:15 +0100

    add navbar icons and skip login screen

--- app/matestack/components/navbar.rb
+++ app/matestack/components/navbar.rb
@@ -1,18 +1,18 @@
 class Components::Navbar < Matestack::Ui::StaticComponent
 
   def prepare
-    @nav_items = {
-      :user_dashboard_path => "Marktplatz",
-      :user_show_profile_path => "Mein Account",
-      :user_chats_path => "Meine Nachrichten",
-      :user_items_path => "Meine Inserate",
-      :user_contacts_path => "Kontaktübersicht"
-    }
+    @nav_items = [
+      {:path => :user_dashboard_path, :text => 'Marktplatz', :icon => 'dashboard'},
+      {:path => :user_show_profile_path, :text => 'Mein Account', :icon => 'user'},
+      {:path => :new_user_chat_path, :text => 'Meine Nachrichten', :icon => 'message'},
+      {:path => :user_items_path, :text => 'Meine Inserate', :icon => 'document'},
+      {:path => :user_contacts_path, :text => 'Kontaktübersicht', :icon => 'phone'}
+    ]
   end
 
   def response
     components {
-      div class: 'navigation shadowed-bg-white pt-5' do
+      div class: 'navigation shadowed-bg-white pt-3' do
 
         transition path: :user_dashboard_path do
           img path: "agora_logo.png", class: 'mb-5 mt-2 logo'
@@ -20,8 +20,11 @@ class Components::Navbar < Matestack::Ui::StaticComponent
 
         heading size: 4, class: 'menu-header mb-4', text: 'Menü'
 
-        @nav_items.each do |path, text|
-          transition path: path,  text: text, class: "#{'nav-active' if text == 'Marktplatz'}"
+        @nav_items.each do |link|
+          transition path: link[:path] do
+            img path: "icons/#{link[:icon]}.svg", class: "navbar-icon"
+            span text: link[:text], class: "#{'nav-active' if link[:text] == 'Marktplatz'}"
+          end
           br
         end
 
